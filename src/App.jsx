9bd5394060a70416566a9c5ff1ebe421709aaea9
Abolfazl <abolfazlm181920@gmail.com>
2023-02-01 14:38:11 +0330

    [list]: add listing logic!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CourseForm from './components/CourseForm'
+import CourseList from './components/CourseList'
 
 const App = () => {
    return (
       <div className='main-container'>
          <h1>My Course List</h1>
          <CourseForm />
+         <CourseList />
       </div>
    )
 }
